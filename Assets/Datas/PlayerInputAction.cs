233caed996ffc134526e560d10b454055421d33f
Ibanez <kevin.ibanezpro@gmail.com>
2020-08-02 21:41:31 +0200

    wip character

---
+++ Assets/Datas/PlayerInputAction.cs
@@ -0,0 +1,234 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Datas/PlayerInputAction.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlayerInputAction : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlayerInputAction()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlayerInputAction"",
+    ""maps"": [
+        {
+            ""name"": ""Character"",
+            ""id"": ""244f4632-5bf1-46f6-9b57-955593ede204"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""37f4bf99-bdf4-4c9f-9b30-499446867042"",
+                    ""expectedControlType"": ""Dpad"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""CameraControl"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""0e16cd39-5bb6-4613-b332-d1c4cce51c9a"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Button"",
+                    ""id"": ""8bb48259-610e-491a-a48c-c3d1db9526fb"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""Keyboard"",
+                    ""id"": ""e8ec5408-7239-47dc-a500-9a0f2d3360b1"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""2339b41b-bde8-44b1-b6c4-3a45dadfba3d"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""5e1e422c-91f9-436b-b4c0-ac7011fb2b22"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""5c38258b-6d08-443f-a350-830365877f44"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""d107bd72-7f1f-4170-9b16-d4805b800b8c"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""3a60e856-204a-470c-abfa-8c3cb9d970bb"",
+                    ""path"": ""<Mouse>/delta"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""CameraControl"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""c453ce87-14b8-4110-b1ec-337148ef102e"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Character
+        m_Character = asset.FindActionMap("Character", throwIfNotFound: true);
+        m_Character_Move = m_Character.FindAction("Move", throwIfNotFound: true);
+        m_Character_CameraControl = m_Character.FindAction("CameraControl", throwIfNotFound: true);
+        m_Character_Jump = m_Character.FindAction("Jump", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Character
+    private readonly InputActionMap m_Character;
+    private ICharacterActions m_CharacterActionsCallbackInterface;
+    private readonly InputAction m_Character_Move;
+    private readonly InputAction m_Character_CameraControl;
+    private readonly InputAction m_Character_Jump;
+    public struct CharacterActions
+    {
+        private @PlayerInputAction m_Wrapper;
+        public CharacterActions(@PlayerInputAction wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_Character_Move;
+        public InputAction @CameraControl => m_Wrapper.m_Character_CameraControl;
+        public InputAction @Jump => m_Wrapper.m_Character_Jump;
+        public InputActionMap Get() { return m_Wrapper.m_Character; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(CharacterActions set) { return set.Get(); }
+        public void SetCallbacks(ICharacterActions instance)
+        {
+            if (m_Wrapper.m_CharacterActionsCallbackInterface != null)
+            {
+                @Move.started -= m_Wrapper.m_CharacterActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_CharacterActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_CharacterActionsCallbackInterface.OnMove;
+                @CameraControl.started -= m_Wrapper.m_CharacterActionsCallbackInterface.OnCameraControl;
+                @CameraControl.performed -= m_Wrapper.m_CharacterActionsCallbackInterface.OnCameraControl;
+                @CameraControl.canceled -= m_Wrapper.m_CharacterActionsCallbackInterface.OnCameraControl;
+                @Jump.started -= m_Wrapper.m_CharacterActionsCallbackInterface.OnJump;
+                @Jump.performed -= m_Wrapper.m_CharacterActionsCallbackInterface.OnJump;
+                @Jump.canceled -= m_Wrapper.m_CharacterActionsCallbackInterface.OnJump;
+            }
+            m_Wrapper.m_CharacterActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+                @CameraControl.started += instance.OnCameraControl;
+                @CameraControl.performed += instance.OnCameraControl;
+                @CameraControl.canceled += instance.OnCameraControl;
+                @Jump.started += instance.OnJump;
+                @Jump.performed += instance.OnJump;
+                @Jump.canceled += instance.OnJump;
+            }
+        }
+    }
+    public CharacterActions @Character => new CharacterActions(this);
+    public interface ICharacterActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+        void OnCameraControl(InputAction.CallbackContext context);
+        void OnJump(InputAction.CallbackContext context);
+    }
+}
---
+++ Assets/Script/Character/CharacterBase.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using UnityEngine;
+
+public class CharacterBase : MonoBehaviour
+{
+
+    [SerializeField] private float m_moveSpeed = 5f;
+    [SerializeField] private Transform m_camera = null ;
+
+    private PlayerInputAction m_inputs;
+    private float m_xCameraRotation;
+
+    private Rigidbody m_rb { get => GetComponent<Rigidbody>(); }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        OnStart();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OnUpdate();
+    }
+
+    private void FixedUpdate()
+    {
+        OnFixedUpdate();
+    }
+
+    protected virtual void OnStart()
+    {
+        m_inputs = new PlayerInputAction();
+        m_inputs.Character.Jump.performed += ctx => Jump();
+        EnableInput();
+    }
+
+    protected virtual void OnUpdate()
+    {
+        Move();
+        CameraControl();
+    }
+
+    protected virtual void OnFixedUpdate()
+    {
+      
+    }
+
+    protected void Move()
+    {
+        Vector3 moveDirection = new Vector3(m_inputs.Character.Move.ReadValue<Vector2>().x, m_rb.velocity.y, m_inputs.Character.Move.ReadValue<Vector2>().y);
+        moveDirection =  (moveDirection.z * transform.forward) + (moveDirection.x * transform.right);
+        m_rb.velocity = moveDirection * m_moveSpeed * Time.deltaTime;
+    }
+
+    protected virtual void CameraControl()
+    {
+        float mouseX = m_inputs.Character.CameraControl.ReadValue<Vector2>().x *5f * Time.deltaTime;
+        float mouseY = m_inputs.Character.CameraControl.ReadValue<Vector2>().y * 5f * Time.deltaTime;
+        transform.Rotate(Vector3.up * mouseX);
+
+        m_xCameraRotation -= mouseY;
+        m_xCameraRotation = Mathf.Clamp(m_xCameraRotation, -90f, 90f);
+
+        m_camera.localRotation = Quaternion.Euler(m_xCameraRotation, m_camera.localRotation.y, m_camera.localRotation.z);
+    }
+
+    protected virtual void Jump()
+    {
+
+        m_rb.AddForce(transform.up * 100f, ForceMode.Impulse);
+    }
+
+    protected virtual void EnableInput()
+    {
+        m_inputs.Enable();
+    }
+
+    protected virtual void DisableInput()
+    {
+        m_inputs.Disable();
+    }
+}
